3879fe8663cde2a3272e2125df2301f9c90c9a31
Devika-2004 <devikawarekar@gmail.com>
2026-03-16 16:32:55 +0530

    Updated features (Prifile page,like,search etc)

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,19 +1,50 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
+const blogSchema = new mongoose.Schema(
+{
   title: {
     type: String,
     required: true
   },
+
   content: {
     type: String,
     required: true
   },
+
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true
-  }
-}, { timestamps: true });
+  },
+
+  // ❤️ Likes
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+
+  // 💬 Comments
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      text: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+
+},
+{ timestamps: true }
+);
 
 module.exports = mongoose.model("Blog", blogSchema);
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 // Create Blog
 router.post("/create", authMiddleware, async (req, res) => {
   try {
+
     const newBlog = new Blog({
       title: req.body.title,
       content: req.body.content,
@@ -13,26 +14,38 @@ router.post("/create", authMiddleware, async (req, res) => {
     });
 
     await newBlog.save();
+
     res.status(201).json({ message: "Blog created successfully" });
 
   } catch (error) {
+
     res.status(500).json({ message: "Server error" });
+
   }
 });
 
-// Get All Blogs (Public)
+
+// Get All Blogs
 router.get("/", async (req, res) => {
   try {
-    const blogs = await Blog.find().populate("author", "name email");
+
+    const blogs = await Blog.find()
+      .populate("author", "name email");
+
     res.json(blogs);
+
   } catch (error) {
+
     res.status(500).json({ message: "Server error" });
+
   }
 });
+
 
 // Get Single Blog
 router.get("/:id", async (req, res) => {
   try {
+
     const blog = await Blog.findById(req.params.id)
       .populate("author", "name email");
 
@@ -41,21 +54,25 @@ router.get("/:id", async (req, res) => {
     }
 
     res.json(blog);
+
   } catch (error) {
+
     res.status(500).json({ message: "Server error" });
+
   }
 });
+
 
 // Update Blog
 router.put("/:id", authMiddleware, async (req, res) => {
   try {
+
     const blog = await Blog.findById(req.params.id);
 
     if (!blog) {
       return res.status(404).json({ message: "Blog not found" });
     }
 
-    // 🔥 Check if logged-in user is blog owner
     if (blog.author.toString() !== req.user.id) {
       return res.status(403).json({ message: "Not authorized" });
     }
@@ -68,13 +85,17 @@ router.put("/:id", authMiddleware, async (req, res) => {
     res.json({ message: "Blog updated successfully" });
 
   } catch (error) {
+
     res.status(500).json({ message: "Server error" });
+
   }
 });
+
 
 // Delete Blog
 router.delete("/:id", authMiddleware, async (req, res) => {
   try {
+
     const blog = await Blog.findById(req.params.id);
 
     if (!blog) {
@@ -90,9 +111,78 @@ router.delete("/:id", authMiddleware, async (req, res) => {
     res.json({ message: "Blog deleted successfully" });
 
   } catch (error) {
+
     res.status(500).json({ message: "Server error" });
+
   }
 });
 
+
+// ❤️ LIKE BLOG
+router.put("/like/:id", authMiddleware, async (req, res) => {
+
+  try {
+
+    const blog = await Blog.findById(req.params.id);
+
+    const userId = req.user.id;
+
+    if (blog.likes.includes(userId)) {
+      blog.likes.pull(userId);
+    } else {
+      blog.likes.push(userId);
+    }
+
+    await blog.save();
+
+    res.json(blog);
+
+  } catch (error) {
+
+    res.status(500).json({ message: "Server error" });
+
+  }
+
+});
+
+
+// 💬 COMMENT BLOG
+router.post("/comment/:id", authMiddleware, async (req, res) => {
+
+  try {
+
+    const blog = await Blog.findById(req.params.id);
+
+    blog.comments.push({
+      user: req.user.id,
+      text: req.body.text
+    });
+
+    await blog.save();
+
+    res.json(blog);
+
+  } catch (error) {
+
+    res.status(500).json({ message: "Server error" });
+
+  }
+
+});
+// GET BLOGS BY USER
+router.get("/user/myblogs", authMiddleware, async (req, res) => {
+  try {
+
+    const blogs = await Blog.find({ author: req.user.id })
+      .populate("author", "name email");
+
+    res.json(blogs);
+
+  } catch (error) {
+
+    res.status(500).json({ message: "Server error" });
+
+  }
+});
 
 module.exports = router;
